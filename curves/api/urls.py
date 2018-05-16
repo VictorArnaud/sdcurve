350c2d7243f567150a1fc003feff472121a9331a
Victor Arnaud <victorhad@gmail.com>
2018-05-16 12:23:46 -0300

    Insert API inside api folder

---
+++ curves/api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+height_curves_patterns_api = [
+    path(
+        'male-months/',
+        views.HeightCurveMaleMonths.as_view(),
+        name='height-male-months'
+    ),
+    path(
+        'male-years/',
+        views.HeightCurveMaleYears.as_view(),
+        name='height-male-years'
+    ),
+    path(
+        'female-months/',
+        views.HeightCurveFemaleMonths.as_view(),
+        name='height-female-months'
+    ),
+    path(
+        'female-years/',
+        views.HeightCurveFemaleYears.as_view(),
+        name='height-female-years'
+    ),
+]
--- curves/urls.py
+++ curves/urls.py
@@ -1,34 +1,11 @@
 from django.urls import path, include
-from . import views
+from .api import urls
 
 app_name = 'curves'
-
-height_curves_patterns = [
-    path(
-        'male-months/',
-        views.HeightCurveMaleMonths.as_view(),
-        name='height-male-months'
-    ),
-    path(
-        'male-years/',
-        views.HeightCurveMaleYears.as_view(),
-        name='height-male-years'
-    ),
-    path(
-        'female-months/',
-        views.HeightCurveFemaleMonths.as_view(),
-        name='height-female-months'
-    ),
-    path(
-        'female-years/',
-        views.HeightCurveFemaleYears.as_view(),
-        name='height-female-years'
-    ),
-]
 
 urlpatterns = [
     path(
         'api/growth-curve/height/',
-        include(height_curves_patterns)
+        include(urls.height_curves_patterns_api)
     ),
 ]
